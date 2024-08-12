7633ae33a4d4a68de9c0bf9c658f7b954584b1d5
Yousef Fathy <105655483+yousseffathy110@users.noreply.github.com>
2024-08-13 01:36:41 +0300

    [389] [Time Beats: 86.43%] [Memory Beats: 40.95%] - LeetPush

--- 389-Find-the-Difference.js
+++ 389-Find-the-Difference.js
@@ -1,6 +1,7 @@
 const findTheDifference = (s, t) => {
-  let first = s.split(\\).sort();
-  let second = t.split(\\).sort();
-  for (let i = 0; i < second.length; i++)
-    if (first[i] !== second[i]) return second[i];
+    let first = s.split(\\).sort(),
+        second = t.split(\\).sort();
+    for (let i = 0; i < second.length; i++)
+        if (first[i] !== second[i]) return second[i];
+    return second[second.length - 1];
 };
